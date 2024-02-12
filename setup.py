38a158dc5546650e6f198c978eee9d64b3633b64
Bhupender <bhupender.contact+github@gmail.com>
2024-02-13 00:36:30 +0530

    Adding AWS Athena Support

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='botofication',
+    version='0.0.1',
+    author='Bhupender',
+    author_email='bhupender.contact+botofication@gmail.com',
+    license="All Rights Are Reserved. (c) https://www.github.com/bhupenderhere",
+    description='botofication is a Python package to help you with your AWS infrastructure automation.',
+    packages=find_packages(),
+    install_requires=[]
+)
